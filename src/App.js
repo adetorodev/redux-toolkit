8fd6e31b3f3e923bea9d27f11c67dc71f924e2ee
Ezekiel Adetoro <adetoroe787@gmail.com>
2023-01-25 20:35:24 -0800

    postlist & add post

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 
-import Counter from './features/counter/Counter'
+import {  BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+
+import { Navbar } from './app/Navbar'
+import { PostsList } from './features/posts/PostsList'
+import { AddPostForm } from './features/posts/AddPostForm'
+import { SinglePostPage } from './features/posts/SinglePostPage'
 
 function App() {
   return (
-    <main className="App">
-      <Counter />
-    </main>
+    <Router>
+    <Navbar />
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<PostsList />}/>
+        <Route path="/add-post" element={<AddPostForm />}/>
+        <Route path="/posts/:postId" element={<SinglePostPage />} />
+      </Routes>
+    </div>
+  </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.post.css';
+import './index.css';
+import { worker } from './api/server'
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+
+async function start() {
+  await worker.start({ onUnhandledRequest: "bypass" });
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+}
+start()
 
 
