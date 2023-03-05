09f26cb93de739abd9e8f01e5163a948fcaef56c
Ezekiel Adetoror <adetoroe787@gmail.com>
2023-03-05 23:42:18 +0100

    reducer & action

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -1,1 +1,27 @@
-console.log("from index")
+const CAKE_ORDERED = "CAKE_ODERED";
+
+function orderCake() {
+  return {
+    type: CAKE_ORDERED,
+    quantity: 1,
+  };
+}
+
+const initialState = {
+    numOfCake: 10
+}
+
+
+
+// (previouSate, action) => newState
+
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case CAKE_ORDERED:
+            return {
+                numOfCake: state.numOfCake - 1,
+            }
+        default: 
+            return state
+    }
+}
